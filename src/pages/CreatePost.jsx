e544a8db93402333a3442f668414086e7bfdd6e0
Rizqi Setiaji <rizqisetiaji9@gmail.com>
2023-01-26 01:24:38 +0700

    Update layout & custom css scrollbar

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -67,64 +67,66 @@ const CreatePost = () => {
 
   return (
     <section className="xl:max-w-6xl mx-auto">
-      <div>
-        <h1 className="font-extrabold text-[#222328] dark:text-white text-[32px]">Create</h1>
-        <p className="mt-2 text-[#666e75] dark:text-[#9aa0a5] text-[14px] max-w-[480px]">Create imaginative and visually stunning images through by DALL-E AI and sare them with the community</p>
-      </div>
+      <div className="max-w-3xl mx-auto">
+        <div>
+          <h1 className="font-extrabold text-[#222328] dark:text-white text-[32px]">Create</h1>
+          <p className="mt-2 text-[#666e75] dark:text-[#9aa0a5] text-[14px] max-w-[480px]">Create imaginative and visually stunning images through by DALL-E AI and sare them with the community</p>
+        </div>
 
-      <form className="mt-14 max-w-3xl" onSubmit={ handleSubmit } autoComplete="off">
-        <div className="flex flex-col gap-5">
-          <FormField 
-            autoFocus
-            labelName="Your name" 
-            type="text" 
-            name="name" 
-            placeholder="Example: John Doe" 
-            value={ form.name }
-            handleChange={ handleChange } 
-          />
+        <form className="mt-14" onSubmit={ handleSubmit } autoComplete="off">
+          <div className="flex flex-col gap-5">
+            <FormField 
+              autoFocus
+              labelName="Your name" 
+              type="text" 
+              name="name" 
+              placeholder="Example: John Doe" 
+              value={ form.name }
+              handleChange={ handleChange } 
+            />
 
-          <FormField 
-            labelName="Type your prompt or" 
-            type="text" 
-            name="prompt" 
-            placeholder="A plush toy robot sitting against a yellow wall" 
-            value={ form.prompt }
-            handleChange={ handleChange }
-            isSurpriseMe
-            handleSurpriseMe={ handleSurpriseMe }
-          />
+            <FormField 
+              labelName="Type your prompt or" 
+              type="text" 
+              name="prompt" 
+              placeholder="A plush toy robot sitting against a yellow wall" 
+              value={ form.prompt }
+              handleChange={ handleChange }
+              isSurpriseMe
+              handleSurpriseMe={ handleSurpriseMe }
+            />
 
-          <div className="relative bg-gray-50 dark:bg-[#0d0c14] border border-gray-300 dark:border-[#201f2c] text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
-            { form.photo ? (
-              <img src={form.photo} alt={form.prompt} className="w-full h-full object-contain" />
-            ) : (
-              <img src={preview} alt="preview" className="w-9/12 h-9/12 object-contain opacity-40 dark:opacity-25 dark:invert" />
-            ) }
+            <div className="relative bg-gray-50 dark:bg-[#0d0c14] border border-gray-300 dark:border-[#201f2c] text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-3 w-full h-auto sm:w-64 sm:h-64 flex justify-center items-center">
+              { form.photo ? (
+                <img src={form.photo} alt={form.prompt} className="w-full h-full object-contain" />
+              ) : (
+                <img src={preview} alt="preview" className="w-9/12 h-9/12 object-contain opacity-40 dark:opacity-25 dark:invert" />
+              ) }
 
-            { generatingImg && (
-              <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
-                <Loader />
-              </div>
-            ) }
+              { generatingImg && (
+                <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
+                  <Loader />
+                </div>
+              ) }
+            </div>
           </div>
-        </div>
 
-        <div className="mt-5 flex gap-5">
-          <button type="button" onClick={generateImage} className="text-white bg-emerald-500 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center">
-            { generatingImg ? 'Generating...' : 'Generate' }
-          </button>
-        </div>
+          <div className="mt-5 flex gap-5">
+            <button type="button" onClick={generateImage} className="text-white bg-emerald-500 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center">
+              { generatingImg ? 'Generating...' : 'Generate' }
+            </button>
+          </div>
 
-        <div className="mt-10">
-          <p className="mt-2 text-[#666e75] dark:text-[#9aa0a5] text-[14px]">
-            Once you have created the image you want, you can share it with others in the community
-          </p>
-          <button type="submit" className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center">
-            { loading ? 'Sharing...' : 'Share with the community' }
-          </button>
-        </div>
-      </form>
+          <div className="mt-10">
+            <p className="mt-2 text-[#666e75] dark:text-[#9aa0a5] text-[14px]">
+              Once you have created the image you want, you can share it with others in the community
+            </p>
+            <button type="submit" className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center">
+              { loading ? 'Sharing...' : 'Share with the community' }
+            </button>
+          </div>
+        </form>
+      </div>
     </section>
   )
 }
